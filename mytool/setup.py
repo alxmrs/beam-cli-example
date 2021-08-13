669505d485b958c63db401301305ef31b9cc55f6
Alex Merose <alxr@google.com>
2021-08-13 12:36:50 -0700

    Updated repo to use my new last name and username.

--- mytool/setup.py
+++ mytool/setup.py
@@ -4,8 +4,8 @@ from setuptools import setup
 setup(
     name='subtool',
     packages=['subtool'],
-    author='Alex Rosengarten',
-    author_email='alxrsngrtn@google.com',
-    url='https://github.com/alxrsngrtn/beam-cli-example',
+    author='Alex Merose',
+    author_email='alxr@google.com',
+    url='https://github.com/alxmrs/beam-cli-example',
     description='A toy python project to demo packaging a dataflow pipeline in a CLI.',
 )
--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup, find_packages
 setup(
     name='mytool',
     packages=find_packages(),
-    author='Alex Rosengarten',
-    author_email='alxrsngrtn@google.com',
-    url='https://github.com/alxrsngrtn/beam-cli-example',
+    author='Alex Merose',
+    author_email='alxr@google.com',
+    url='https://github.com/alxmrs/beam-cli-example',
     description='A toy python project to demo packaging a dataflow pipeline in a CLI.',
     install_requirements=['apache-beam[gcp]'],
     scripts=['mytool/boom']
